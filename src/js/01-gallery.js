1ec170b4d9f4c7d17af6af6c5016817f2ed96ebe
Denys Yefimkin <iefimkindenis@gmail.com>
2022-07-22 21:41:32 +0300

    update task 1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,29 @@
-// Add imports above this line
 import { galleryItems } from './gallery-items';
+import SimpleLightbox from "simplelightbox";
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
 // Change code below this line
 
-console.log(galleryItems);
+const galleryRef = document.querySelector('.gallery');
+
+function galleryRender(arrayOfImages) {
+    const arrayOfImg = [];
+    galleryItems.map(({ preview, original, description }) => {
+        arrayOfImg.push(`<a class="gallery__item" href="${original}">
+                            <img class="gallery__image" 
+                            src="${preview}" 
+                            alt="${description}" />
+                        </a>`);
+    });
+    galleryRef.insertAdjacentHTML('beforeend', arrayOfImg.join(''));
+}
+galleryRender(galleryItems);
+
+
+
+let lightbox = new SimpleLightbox('.gallery a', {
+    captionsData: 'alt',
+    captionDelay: 250,
+    overlayOpacity: 0.9,
+    loop: true,
+});
